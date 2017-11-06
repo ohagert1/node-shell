fc826df2958f912ed9eaaa877f33cf16cfa85c32
ohagert1 <ohagerty@gmail.com>
2017-11-06 13:56:12 -0500

    added ls functionality

--- bash.js
+++ bash.js
@@ -2,10 +2,17 @@ const cmd = require('./commands');
 
 process.stdin.on('data', function(input) {
   var userInput = input.toString().trim().toUpperCase();
-  if(userInput === 'DATE') {
-    cmd.logDate();
-  } else if(userInput === 'PWD') {
-    cmd.pwd();
+  switch(userInput) {
+    case 'DATE':
+      cmd.logDate();
+      break;
+    case 'PWD':
+      cmd.pwd();
+      break;
+    case 'LS':
+      cmd.ls();
+      break;
+
   }
 });
 
--- commands.js
+++ commands.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 module.exports = {
   pwd: function() {
     process.stdout.write(process.cwd());
@@ -10,5 +12,15 @@ module.exports = {
   logDate: function() {
     var date = new Date;
     process.stdout.write(date.toString());
+  },
+
+  ls: function() {
+    fs.readdir('.', function(err, files) {
+      if (err) throw err;
+      files.forEach(function(file) {
+        process.stdout.write(file.toString() + "\n");
+      });
+      process.stdout.write("prompt > ");
+    });
   }
 };
