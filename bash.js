47126a14ded8336e321127502b8164a0a5167a98
ohagert1 <ohagerty@gmail.com>
2017-11-06 12:44:21 -0500

    added date and pwd functionality and commands.js file

--- bash.js
+++ bash.js
@@ -1,1 +1,27 @@
 // Start of the node shell workshop
+function pwd() {
+  process.stdout.write(process.cwd());
+}
+
+
+function fullDate() {
+  var date = new Date;
+  return date.toString();
+}
+// console.log(cwd);
+// console.log(fullDate());
+
+function logDate() {
+  process.stdout.write(fullDate());
+}
+
+process.stdin.on('data', function(input) {
+  var userInput = input.toString().trim().toUpperCase();
+  if(userInput === 'DATE') {
+    logDate();
+  } else if(userInput === 'PWD') {
+    pwd();
+  }
+});
+
+
